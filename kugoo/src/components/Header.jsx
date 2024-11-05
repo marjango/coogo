d00a7d2331bc5aa82de936038cfd6e46d6d6da64
marjango <misoo00up@gmail.com>
2024-11-05 20:07:40 +0500

    aaaa

--- kugoo/src/components/Header.jsx
+++ kugoo/src/components/Header.jsx
@@ -4,6 +4,10 @@ import viber from '../img/viber.png'
 import whatsapp from '../img/whatsapp.png'
 import telegram from '../img/telegram.png'
 import plus from '../img/plus.png'
+import balance from '../img/balance.png'
+import heart from '../img/Icon/Outlined/Heart.png'
+import shoppingCart from '../img/shopping-cart.png'
+
 
 function Header() {
     return (
@@ -26,7 +30,15 @@ function Header() {
                 </div>
             </div>
             <div className="main_header">
-
+                <h1>KUGOO</h1>
+                <button className="catalogBtn">Каталог</button>
+                <input type="text" placeholder="Искать самокат KUGOO"></input>
+                <div className="cart_content">
+                    <img src={balance} alt="" />
+                    <img src={heart} alt="" />
+                    <img src={shoppingCart} alt="" />
+                    <p>Корзина</p>
+                </div>
             </div>
             <div className="brief_header">
 
